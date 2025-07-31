36a1f4101e5af21e5fe93dd21c53c5754847ba62
Alexey Karateev <52000764+IVENTEYE@users.noreply.github.com>
2025-08-01 00:06:08 +0600

    Add files via upload

--- src/js/app.js
+++ src/js/app.js
@@ -3,33 +3,56 @@ import Swiper from 'swiper/bundle';
 
 checkingFunctions.isWebp();
 
-console.log("Hello");
+const header = document.querySelector('.header'),
+  iconMenu = document.querySelector('.menu__icon'),
+  menuBody = document.querySelector('.menu');
 
-// const swiperSliderLeft = new Swiper('.slider-left', {
-//   speed: 400,
-//   spaceBetween: 100,
-// });
-
-if (document.querySelector(".slider-left")) {
-    new Swiper('.slider-left', {
-        speed: 400,
-        spaceBetween: 30,
-        autoplay: {
-            delay: 5000,
-        },
-        effect: "fade",
-        loop: true,
-    });
+if (iconMenu) {
+  iconMenu.addEventListener('click', () => {
+    iconMenu.classList.toggle('_active');
+    menuBody.classList.toggle('_active');
+    document.body.classList.toggle('lock');
+  });
 }
 
-if (document.querySelector(".slider-right")) {
-    new Swiper('.slider-right', {
-        speed: 400,
-        spaceBetween: 30,
-        autoplay: {
-            delay: 4500,
-        },
-        effect: "fade",
-        loop: true,
-    });
+const removeClassMenu = () => {
+  iconMenu.classList.remove('_active');
+  menuBody.classList.remove('_active');
+};
+
+const callback = function (entries, observer) {
+  if (entries[0].isIntersecting) {
+    header.classList.remove('_scroll');
+  } else {
+    header.classList.add('_scroll');
+  }
+};
+
+window.addEventListener('scroll', () => {
+  const headerObserver = new IntersectionObserver(callback);
+  headerObserver.observe(header);
+});
+
+if (document.querySelector('.slider-left')) {
+  new Swiper('.slider-left', {
+    speed: 400,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 5000,
+    },
+    effect: 'fade',
+    loop: true,
+  });
+}
+
+if (document.querySelector('.slider-right')) {
+  new Swiper('.slider-right', {
+    speed: 400,
+    spaceBetween: 30,
+    autoplay: {
+      delay: 4500,
+    },
+    effect: 'fade',
+    loop: true,
+  });
 }
